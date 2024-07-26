613b6dbcf46b5243c5d2b895641f75d28a74319e
Hao <truonggiahao45@gmail.com>
2024-07-26 20:20:58 +0700

    Let's Go Chapter 3.4

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,18 +7,44 @@ import (
 	"os"
 )
 
+// Create a new application type
+type application struct {
+	errorLog *log.Logger
+	infoLog  *log.Logger
+}
+
 func main() {
+	//add a new flag named addr with default port number 4000 to change at run time
 	addr := flag.String("addr", ":4000", "HTTP network addr")
+
+	//parse the command-line flags from os.Args. Must be call after all flags are defined
+	//and before flags are accessed by the program
 	flag.Parse()
-	mux := http.NewServeMux()
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
-	mux.Handle("/static/", http.StripPrefix("/static/", fileServer))
-	mux.HandleFunc("/", home)
-	mux.HandleFunc("/snippet/view", snippetView)
-	mux.HandleFunc("/snippet/crate", snippetCreate)
+
+	//create new logger with log.New(out io.Writer,prefix String, flags int)
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	//Create a new *http.ServeMux which act as a router
+	mux := http.NewServeMux()
+
+	//Initialize a new instance of application named app
+	app := &application{
+		infoLog:  infoLog,
+		errorLog: errLog,
+	}
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	srv := http.Server{
+		Addr:     *addr,
+		ErrorLog: errLog,
+		Handler:  mux,
+	}
+	mux.Handle("/static/", http.StripPrefix("/static/", fileServer))
+	mux.HandleFunc("/", app.home)
+	mux.HandleFunc("/snippet/view", app.snippetView)
+	mux.HandleFunc("/snippet/crate", app.snippetCreate)
+
 	infoLog.Printf("Starting server on %s", *addr)
-	err := http.ListenAndServe(*addr, mux)
+	err := srv.ListenAndServe()
 	errLog.Fatal(err)
 }
